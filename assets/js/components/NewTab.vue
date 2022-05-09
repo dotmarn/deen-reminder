14b5ec5bdffed7ced4ff1e03b5039875d5b28115
Kasim Ridwan <kasimsheyi20@gmail.com>
2022-05-09 13:57:50 +0100

    clean up

--- assets/js/components/NewTab.vue
+++ assets/js/components/NewTab.vue
@@ -1,22 +1,38 @@
 <template>
-    <div class="text-white">
-        Hello world
+    <div class="flex flex-col h-screen">
+        <div class="grid place-items-center my-auto">
+            <div class="shadow-2xl rounded-lg bg-[#242424] border-2 border-[#42B883] text-center p-8 space-y-5" v-if="quote">
+                <h1 class="text-white italic font-Monts text-3xl">{{quote.roman}}</h1>
+                <h1 class="text-[#ebebeb99] font-Karla font-bold text-3xl">{{quote.english}}</h1>
+                <p class="italic font-Karla text-[#42B883]">- {{quote.source}}</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'NewTab',
     data() {
         return {
-            active: true
+            quote: {}
         }
     },
     created() {
-        console.log('got here')
+        this.fetchQuote();
     },
     methods: {
-
+        async fetchQuote() {
+            let url = "json/data.json";
+            await axios.get(url).then((res) => {
+                let data = res.data;
+                let random = Math.floor(Math.random() * data.length);
+                this.quote = data[random];
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
     }
 }
 </script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require('laravel-mix');
 
 mix.setPublicPath('./')
     .js('assets/js/newtab.js', 'dist/js').vue()
+    .copy('assets/json', 'dist/json')
     .copy('assets/images/', 'dist/images')
     .options({
         processCssUrls: false
