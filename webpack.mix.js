8624b8f9a484f868324b96ad4eb49190119b512d
Kasim Ridwan <kasimsheyi20@gmail.com>
2022-05-09 15:36:36 +0100

    clean up

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,6 @@ let mix = require('laravel-mix');
 
 mix.setPublicPath('./')
     .js('assets/js/newtab.js', 'dist/js').vue()
-    .copy('assets/json', 'dist/json')
     .copy('assets/images/', 'dist/images')
     .options({
         processCssUrls: false
